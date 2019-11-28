6baf7331166a4e744b77c484b385fca305ff13ab
Dan Hromada <hromada.dan@gmail.com>
2019-11-28 12:41:39 +0100

    Fix heureka api url has change

--- src/bg/background.js
+++ src/bg/background.js
@@ -3,37 +3,42 @@
 // var settings = new Store("settings", {
 //     "sample_setting": "This is how you use Store.js to remember values"
 // });
-console.log('alive');
+console.log("alive");
 
 chrome.extension.onMessage.addListener((request, sender, sendResponse) => {
-    const { query, payload } = request;
-    callToHeureka(query, payload).then(sendResponse);
+  const { query, payload } = request;
+  callToHeureka(query, payload).then(sendResponse);
 
-    return true;
+  return true;
 });
 
 async function callToHeureka(query, payload) {
-    switch (query) {
-        case 'Najdi mi prosimtě tohle zbožíčko': {
-            return await searchProductsOnHeureka(payload);
-        }
+  switch (query) {
+    case "Najdi mi prosimtě tohle zbožíčko": {
+      return await searchProductsOnHeureka(payload);
     }
+  }
 }
 
 async function searchProductsOnHeureka(term) {
-    // Hack: Using Heureka's autocomplete suggestions
-    const result = await fetch(
-        `https://www.heureka.cz/direct/ajax/search-suggester?term=${term}`,
-    );
+  // Hack: Using Heureka's autocomplete suggestions
+  const result = await fetch(
+    // ... but now its regular api, ❤️ Heureka
+    `https://api.heureka.cz/head-gateway/search?term=${term}`
+  );
 
+  try {
     const {
-        products: { result: foundProducts },
+      products: { result: foundProducts },
     } = await result.json();
-    console.log('Products found on Heureka', foundProducts);
+    console.log("Products found on Heureka", foundProducts);
 
     // Using Heureka score. The number is magic for us, but seems to work well
     const relevantProducts = foundProducts.filter(({ score }) => score > 10500);
-    console.log('Hopefully relevant products', relevantProducts);
+    console.log("Hopefully relevant products", relevantProducts);
 
     return relevantProducts;
+  } catch (error) {
+    console.error(`It seems like  Heureka's API has change`);
+  }
 }
