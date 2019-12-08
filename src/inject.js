9b52df262c634628495ae0106f1853d02ce31e76
Dan Hromada <hromada.dan@gmail.com>
2019-12-08 11:59:47 +0100

    Add icon and fix edge cases (no products)

--- src/inject.js
+++ src/inject.js
@@ -34,9 +34,8 @@ async function liveTimeBaby() {
   if (productPrice)
     console.log({ productName, productPrice, heurekaPrices, foundProducts });
 
-  const productsAreNotCheaper = Boolean(
-    heurekaPrices.find(price => price >= productPrice)
-  );
+  const productsAreNotCheaper =
+    Boolean(heurekaPrices.find(price => price < productPrice)) === false;
 
   if (productsAreNotCheaper) {
     console.log("Products are not cheaper");
@@ -80,6 +79,15 @@ const makeHeurekaRoot = () => {
   return heurekaContainer;
 };
 
+const extensionIcon = `
+<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <g>
+    <path d="M13.5888 0.922928L20.8164 5.09465C21.7933 5.65854 22.3951 6.70061 22.3951 7.82828V16.1717C22.3951 17.2994 21.7933 18.3415 20.8164 18.9054L13.5888 23.0771C12.6119 23.641 11.4082 23.641 10.4312 23.0771L3.20371 18.9054C2.22676 18.3415 1.625 17.2994 1.625 16.1717V7.82828C1.625 6.70061 2.22676 5.65854 3.20371 5.09464L10.4312 0.922928C11.4082 0.359024 12.6119 0.359024 13.5888 0.922928ZM12.8696 2.16797C12.3377 1.86096 11.6824 1.86096 11.1505 2.16797L3.92299 6.33968C3.39108 6.6467 3.06335 7.21413 3.06335 7.82828V16.1717C3.06335 16.7859 3.39108 17.3533 3.92299 17.6603L11.1505 21.832C11.6824 22.139 12.3377 22.139 12.8696 21.832L20.0971 17.6603C20.629 17.3533 20.9567 16.7859 20.9567 16.1717V7.82828C20.9567 7.21413 20.629 6.6467 20.0971 6.33968L12.8696 2.16797Z" fill="white" stroke="white"/>
+    <path fill-rule="evenodd" clip-rule="evenodd" d="M15.6122 12.5397C15.8636 12.5421 16.0694 12.7479 16.0719 12.9994L16.0894 14.8205C16.0906 14.9412 16.0437 15.0566 15.9591 15.1411C15.8746 15.2257 15.7592 15.2726 15.6385 15.2714L13.8173 15.2539C13.5658 15.2515 13.3601 15.0457 13.3576 14.7943C13.3552 14.5428 13.5571 14.341 13.8086 14.3434L14.5194 14.3502L11.4856 11.3587L9.99534 12.6593C9.81647 12.8154 9.54453 12.8037 9.37341 12.6326L7.07499 10.3344C6.89548 10.1549 6.8927 9.86659 7.06879 9.69049C7.24488 9.51439 7.53315 9.51714 7.71266 9.69664L9.707 11.6908L11.195 10.3922C11.3729 10.2369 11.6432 10.2476 11.8146 10.4166L15.1684 13.7236L15.1613 12.9906C15.1589 12.7392 15.3608 12.5373 15.6122 12.5397Z" fill="white" stroke="white" stroke-width="0.5" stroke-linecap="round" stroke-linejoin="round"/>
+  </g>
+</svg>
+`;
+
 // Yeah, old fashion style
 const makeHeurekaBox = ({ products, productsAreNotCheaper }) => {
   const box = document.createElement("div");
@@ -91,7 +99,7 @@ const makeHeurekaBox = ({ products, productsAreNotCheaper }) => {
 
   if (!products.length) {
     title.classList.add("HeurekaBox__Header--no-result");
-    title.innerHTML = `Ve srovnávači jsme nenašli podobný produkt`;
+    title.innerHTML = `Ve srovnávači jsme nenašli podobný produkt.`;
     box.appendChild(title);
     // TODO add link to verify manually
     return box;
@@ -101,10 +109,12 @@ const makeHeurekaBox = ({ products, productsAreNotCheaper }) => {
     title.classList.add("HeurekaBox__Title--no-result");
     title.innerHTML = `Cena tohoto produktu je ve srovnávači vyšší.`;
     box.appendChild(title);
-    // return box; DO not return, show list anyway
+    return box;
   }
 
   title.innerText = "Ve srovnávači jsme našli tyto produkty";
+  title.innerHTML += extensionIcon;
+
   box.appendChild(title);
 
   const list = document.createElement("div");
