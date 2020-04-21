9485e8ba8442f25f51fcf1160b9ca23f37689f1c
Strajk <strajk@me.com>
2020-04-21 16:56:37 +0200

    Use Adblock (uBlock Origin) and Cookies-banner-block (I don't care about cookies) in Cypress for cleaner results

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,10 +1,14 @@
 const extensionLoader = require("cypress-browser-extension-plugin/loader");
 
+require("child_process").fork("./cypress/other-extensions/unpack.js");
+
 /**
  * @type {Cypress.PluginConfig}
  */
 module.exports = on => {
   on("before:browser:launch", extensionLoader.load(
     { source: "./extension", alias: "heureka-extension" },
+    { source: "./cypress/other-extensions/unpacked/i-dont-care-about-cookies", alias: "cookies" },
+    { source: "./cypress/other-extensions/unpacked/ublock-origin", alias: "ublock", skipHooks: true },
   ));
 };
