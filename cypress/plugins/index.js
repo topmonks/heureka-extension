169164aff7f7c2b04cbdbe69faac73f872648a8b
Strajk <strajk@me.com>
2020-04-23 19:47:28 +0200

    Add cypress-skip-and-only-ui

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,11 +1,16 @@
+// Beware: only & skip buttons will appear only after finishing all tests
+// https://github.com/bahmutov/cypress-skip-and-only-ui/issues/74
+const task = require("cypress-skip-and-only-ui/task");
 const extensionLoader = require("cypress-browser-extension-plugin/loader");
 
+// TODO: Nicer
 require("child_process").fork("./cypress/other-extensions/unpack.js");
 
 /**
  * @type {Cypress.PluginConfig}
  */
-module.exports = on => {
+module.exports = (on, config) => { // eslint-disable-line no-unused-vars
+  on("task", task);
   on("before:browser:launch", extensionLoader.load(
     { source: "./extension", alias: "heureka-extension" },
     { source: "./cypress/other-extensions/unpacked/i-dont-care-about-cookies", alias: "cookies" },
