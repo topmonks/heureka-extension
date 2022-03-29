48365a9f212d137781d5adefa43850fc6c22db41
Pavel 'Strajk' Dolecek <strajk@me.com>
2022-03-29 20:24:01 +0200

    chore: openAllExamplesInBrowser adjustment

--- utils/openAllExamplesInBrowser.js
+++ utils/openAllExamplesInBrowser.js
@@ -2,8 +2,15 @@ const { execSync } = require("child_process");
 const scrawlers = require("../src/scrawlers");
 const simpleBraceExpansion = require("./simpleBraceExpansion");
 
+const SPECIFIC_BROWSER = "Google Chrome Dev"; // feel free to adjust to your needs, or clear to use default browser
+
 for (const shop of Object.values(scrawlers)) {
   for (const url of simpleBraceExpansion(shop.origin)) {
-    execSync(`open ${url}/${shop.exampleProductPath}`);
+    const fullUrl = `${url}/${shop.exampleProductPath}`;
+    execSync(
+      SPECIFIC_BROWSER ?
+        `open -a "${SPECIFIC_BROWSER}" ${fullUrl}` :
+        `open ${fullUrl}`
+    );
   }
 }
